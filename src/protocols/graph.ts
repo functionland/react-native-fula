f7e909780eab6dc19ed3a07a58a14be5b263c2cc
Mehdi Balouchi <mehdibalouchi@gmail.com>
2022-05-18 21:35:30 +0430

    Change `graphql` to unmarshal binary data

--- src/protocols/graph.ts
+++ src/protocols/graph.ts
@@ -1,4 +1,6 @@
 import Fula from '../interfaces/fulaNativeMudole';
+import { Result } from '@functionland/graph-protocol';
+import 'fastestsmallesttextencoderdecoder';
 
 /**
  * Run a graphql operation on the Box using the graph protocol
@@ -6,16 +8,25 @@ import Fula from '../interfaces/fulaNativeMudole';
  * @param variableValues
  * @returns
  */
-export const graphql = (query: string, variableValues: Record<string, any>): Promise<any> => {
+export const graphql = (
+  query: string,
+  variableValues: Record<string, any>
+): Promise<any> => {
   return new Promise(async (resolve, reject) => {
-    try{
-        const values = JSON.stringify(variableValues)
-        const resJsonStr = await Fula.graphQL(query, values)
-        const res = JSON.parse(resJsonStr)
-        resolve(res)
+    try {
+      const values = JSON.stringify(variableValues);
+      const buf = await Fula.graphQL(query, values);
+      const res = Result.fromBinary(Uint8Array.from(buf));
+      
+      if (res.dataOrError.oneofKind === 'error') {
+        reject(Result.toJson(res));
+        return
+      }
+      else {
+        resolve(Result.toJson(res));
+      }
+    } catch (e) {
+      reject(e);
     }
-    catch (e){
-        reject(e)
-    }
-  })
+  });
 };
