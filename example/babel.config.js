c1eb86c55b7c9e672f48774407d652a5dbad30f2
ehsan shariati <ehsan6sha@gmail.com>
2024-12-07 17:37:01 -0500

    updated polkadot libraries

--- example/babel.config.js
+++ example/babel.config.js
@@ -4,6 +4,7 @@ const pak = require('../package.json');
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    '@babel/plugin-transform-class-static-block',
     '@babel/plugin-proposal-export-namespace-from',
     [
       'module-resolver',
